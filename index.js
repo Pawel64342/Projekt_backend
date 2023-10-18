ea0db1f0821ae57335bd86816570d9c35f820eba
Pawel64342 <wsbp64342>
2023-10-18 20:11:06 +0200

    Zmiany

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+const config = require('./config');
 var Express=require("express");
 var Mongoclient=require("mongodb").MongoClient;
 var cors=require("cors");
@@ -6,8 +8,9 @@ const multer=require("multer")
 var app=Express()
 
 app.use(cors())
-var CONNECTION_STRING="mongodb+srv://Pawel64342:Student.002@cluster0.poobja8.mongodb.net/?retryWrites=true&w=majority"
-
+// var CONNECTION_STRING="mongodb+srv://Pawel64342:Student.002@cluster0.poobja8.mongodb.net/?retryWrites=true&w=majority"
+let c=config
+var CONNECTION_STRING=config
 var DATABASENAME="todoappdb"
 var database;
 
@@ -15,7 +18,7 @@ const port = process.env.PORT || 9003;
 app.listen(port,()=>{
     Mongoclient.connect(CONNECTION_STRING,(error,client)=>{
         database=client.db(DATABASENAME);
-        console.log("Mongo DB connection success ",port)
+        console.log("Mongo DB connection success ",c,port)
     }
         )
 })
@@ -25,7 +28,7 @@ app.listen(port,()=>{
 app.get('/api/todoapp/GetNotes',(request,response)=>{
     database.collection("todoappcollection").find({}).toArray((error,result)=>{
         response.send(result)
-        console.log("result ",result)
+        console.log("result12 ",result)
     })
 })
 
@@ -33,7 +36,10 @@ app.post('/api/todoapp/AddNotes',multer().none(),(request,response)=>{
     database.collection("todoappcollection").count({},function(error,numOfDocs){
     database.collection("todoappcollection").insertOne({
         id:(numOfDocs+1).toString(),
-        description:request.body.newNotes
+        description:request.body.newNotes,
+        positive:request.body.positive,
+        negative:request.body.negative
+
     })
     response.json("added Succesfully")
 })
@@ -44,4 +50,73 @@ app.delete('/api/todoapp/DeleteNotes',(request,response)=>{
         id:request.query.id
     })
     response.json("Delete Succsesfullt")
+    console.log("delete ")
+
 })
+
+
+app.put('/api/todoapp/UpdateNotes', multer().none(), (request, response) => {
+    const id = request.body.id;
+    const newDescription = request.body.newDescription;
+    const positive = request.body.positive;
+    const negative = request.body.negative;
+console.log("update")
+
+    if (!id || !newDescription) {
+        response.status(400).json({ message: "Update a record." });
+        return;
+    }
+
+    database.collection("todoappcollection").updateOne(
+        { id: id },
+        { $set: { description: newDescription ,
+            positive: positive,
+            negative: negative
+        }
+        
+    },
+
+
+        (error, result) => {
+            if (result.matchedCount === 0) {
+                response.status(404).json({ message: "Record not found." });
+            } else {
+                response.json({ message: "Record updated successfully." });
+            }
+        }
+    );
+});
+
+
+
+app.put('/api/todoapp/UpdateNumber', multer().none(), (request, response) => {
+    const id = request.body.id;
+   
+    const positive = request.body.positive;
+    const negative = request.body.negative;
+console.log("update")
+
+    if (!id ) {
+        response.status(400).json({ message: "Update a record." });
+        return;
+    }
+
+    database.collection("todoappcollection").updateOne(
+        { id: id },
+        { $set: {
+            positive: positive,
+            negative: negative
+        }
+        
+    },
+
+
+        (error, result) => {
+            if (result.matchedCount === 0) {
+                response.status(404).json({ message: "Record1 not found." });
+            } else {
+                response.json({ message: "Record1 updated successfully." });
+            }
+        }
+    );
+});
